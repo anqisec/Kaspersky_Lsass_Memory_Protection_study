099cb230833c6ea1d5ad7cb0e5deddd950239347
12138 <48377190+wqreytuk@users.noreply.github.com>
2023-08-28 17:39:34 +0800

    Create svchost节点过滤流程.asm

---
+++ svchost节点过滤流程.asm
@@ -0,0 +1,331 @@
+kd> pa @$ra
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c077 rsp=ffff980b6f616858 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf5f7:
+fffff803`8128c077 48895808        mov     qword ptr [rax+8],rbx ds:002b:ffff980b`6f616860=0000000000000310
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c07b rsp=ffff980b6f616858 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf5fb:
+fffff803`8128c07b 48896810        mov     qword ptr [rax+10h],rbp ds:002b:ffff980b`6f616868=ffff980b6f616929
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c07f rsp=ffff980b6f616858 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf5ff:
+fffff803`8128c07f 48897018        mov     qword ptr [rax+18h],rsi ds:002b:ffff980b`6f616870=ffffd00d323eef08
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c083 rsp=ffff980b6f616858 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf603:
+fffff803`8128c083 48897820        mov     qword ptr [rax+20h],rdi ds:002b:ffff980b`6f616878=0000000000000000
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c087 rsp=ffff980b6f616858 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf607:
+fffff803`8128c087 4154            push    r12
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c089 rsp=ffff980b6f616850 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf609:
+fffff803`8128c089 4156            push    r14
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c08b rsp=ffff980b6f616848 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf60b:
+fffff803`8128c08b 4157            push    r15
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c08d rsp=ffff980b6f616840 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf60d:
+fffff803`8128c08d 4883ec30        sub     rsp,30h
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c091 rsp=ffff980b6f616810 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf611:
+fffff803`8128c091 418af9          mov     dil,r9b
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c094 rsp=ffff980b6f616810 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf614:
+fffff803`8128c094 4d8bf0          mov     r14,r8
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c097 rsp=ffff980b6f616810 rbp=ffff980b6f616929
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf617:
+fffff803`8128c097 488bea          mov     rbp,rdx
+rax=ffff980b6f616858 rbx=0000000000000310 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c09a rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf61a:
+fffff803`8128c09a 488bd9          mov     rbx,rcx
+rax=ffff980b6f616858 rbx=ffffd00d323f44d0 rcx=ffffd00d323f44d0
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c09d rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f6167f0 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf61d:
+fffff803`8128c09d e8ca190000      call    klflt!ComrUnregisterProvider+0x10fec (fffff803`8128da6c)
+rax=ffffe3af4fa69701 rbx=ffffd00d323f44d0 rcx=7ba420c7f0f70000
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0a2 rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f616808 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040286
+klflt!ComrUnregisterProvider+0xf622:
+fffff803`8128c0a2 84c0            test    al,al
+rax=ffffe3af4fa69701 rbx=ffffd00d323f44d0 rcx=7ba420c7f0f70000
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0a4 rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f616808 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040202
+klflt!ComrUnregisterProvider+0xf624:
+fffff803`8128c0a4 0f84a1000000    je      klflt!ComrUnregisterProvider+0xf6cb (fffff803`8128c14b) [br=0]
+rax=ffffe3af4fa69701 rbx=ffffd00d323f44d0 rcx=7ba420c7f0f70000
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0aa rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f616808 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040202
+klflt!ComrUnregisterProvider+0xf62a:
+fffff803`8128c0aa 4084ff          test    dil,dil
+rax=ffffe3af4fa69701 rbx=ffffd00d323f44d0 rcx=7ba420c7f0f70000
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0ad rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f616808 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf62d:
+fffff803`8128c0ad 7514            jne     klflt!ComrUnregisterProvider+0xf643 (fffff803`8128c0c3) [br=0]
+rax=ffffe3af4fa69701 rbx=ffffd00d323f44d0 rcx=7ba420c7f0f70000
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0af rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f616808 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf62f:
+fffff803`8128c0af 418b5630        mov     edx,dword ptr [r14+30h] ds:002b:ffffba83`a6afbb6c=0000033c
+rax=ffffe3af4fa69701 rbx=ffffd00d323f44d0 rcx=7ba420c7f0f70000
+rdx=000000000000033c rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0b3 rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f616808 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf633:
+fffff803`8128c0b3 488bcb          mov     rcx,rbx
+rax=ffffe3af4fa69701 rbx=ffffd00d323f44d0 rcx=ffffd00d323f44d0
+rdx=000000000000033c rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0b6 rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=ffffba83a6afbb3c  r9=0000000000000000 r10=fffff8037c81d8c0
+r11=ffff980b6f616808 r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf636:
+fffff803`8128c0b6 e835010000      call    klflt!ComrUnregisterProvider+0xf770 (fffff803`8128c1f0)
+rax=ffffd00d31fd7000 rbx=ffffd00d323f44d0 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0bb rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040286
+klflt!ComrUnregisterProvider+0xf63b:
+fffff803`8128c0bb 84c0            test    al,al
+rax=ffffd00d31fd7000 rbx=ffffd00d323f44d0 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c0bd rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf63d:
+fffff803`8128c0bd 0f8488000000    je      klflt!ComrUnregisterProvider+0xf6cb (fffff803`8128c14b) [br=1]
+rax=ffffd00d31fd7000 rbx=ffffd00d323f44d0 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c14b rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf6cb:
+fffff803`8128c14b 32c0            xor     al,al
+rax=ffffd00d31fd7000 rbx=ffffd00d323f44d0 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c14d rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf6cd:
+fffff803`8128c14d 488b5c2450      mov     rbx,qword ptr [rsp+50h] ss:0018:ffff980b`6f616860=0000000000000310
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c152 rsp=ffff980b6f616810 rbp=0000000000000000
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf6d2:
+fffff803`8128c152 488b6c2458      mov     rbp,qword ptr [rsp+58h] ss:0018:ffff980b`6f616868=ffff980b6f616929
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c157 rsp=ffff980b6f616810 rbp=ffff980b6f616929
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf6d7:
+fffff803`8128c157 488b742460      mov     rsi,qword ptr [rsp+60h] ss:0018:ffff980b`6f616870=ffffd00d323f4548
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c15c rsp=ffff980b6f616810 rbp=ffff980b6f616929
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf6dc:
+fffff803`8128c15c 488b7c2468      mov     rdi,qword ptr [rsp+68h] ss:0018:ffff980b`6f616878=0000000000000000
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c161 rsp=ffff980b6f616810 rbp=ffff980b6f616929
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei pl zr na po nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040246
+klflt!ComrUnregisterProvider+0xf6e1:
+fffff803`8128c161 4883c430        add     rsp,30h
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c165 rsp=ffff980b6f616840 rbp=ffff980b6f616929
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf6e5:
+fffff803`8128c165 415f            pop     r15
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c167 rsp=ffff980b6f616848 rbp=ffff980b6f616929
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf6e7:
+fffff803`8128c167 415e            pop     r14
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c169 rsp=ffff980b6f616850 rbp=ffff980b6f616929
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf6e9:
+fffff803`8128c169 415c            pop     r12
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128c16b rsp=ffff980b6f616858 rbp=ffff980b6f616929
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xf6eb:
+fffff803`8128c16b c3              ret
+rax=ffffd00d31fd7000 rbx=0000000000000310 rcx=0000000000000010
+rdx=0000000000000000 rsi=ffffd00d323f4548 rdi=0000000000000000
+rip=fffff8038128b4b5 rsp=ffff980b6f616860 rbp=ffff980b6f616929
+ r8=0000000000000003  r9=0000000000000004 r10=fffff8037c820701
+r11=000000000000033c r12=ffff980b6f616900 r13=ffffba83a6afbb3c
+r14=ffffba83a6afbb3c r15=fffff803812d5e98
+iopl=0         nv up ei ng nz na pe nc
+cs=0010  ss=0018  ds=002b  es=002b  fs=0053  gs=002b             efl=00040282
+klflt!ComrUnregisterProvider+0xea35:
+fffff803`8128b4b5 84c0            test    al,al
